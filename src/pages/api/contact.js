51159dbac27bccb240b5b2d46e5ac8ede0e4229f
Md Abdur Rahman Sifat <mdabdurrahmansifat@gmail.com>
2024-03-26 18:36:14 +0600

    error fixed

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -41,10 +41,14 @@ const handler = async(req, res) => {
         return res.status(200).json({ message: "Message sent successfully" });
       }
       )
+      .catch((error) => {
+        console.error(error);
+        return res.status(500).json({ message: "An error occurred while sending the message" });
+      });
     }
     catch(error){
       console.error(error);
-      return res.status(500).json({message: "An error occured while sending the message"});
+      return res.status(500).json({message: "An error occurred while sending the message"});
     }
 
   }
