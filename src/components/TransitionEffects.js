2230ea5dcab5bb6b76d63888ead956d7be1becd0
Md Abdur Rahman Sifat <mdabdurrahmansifat@gmail.com>
2024-08-22 23:26:42 +0600

    update readme.md

--- src/components/TransitionEffects.js
+++ src/components/TransitionEffects.js
@@ -4,7 +4,7 @@ function TransitionEffects() {
  return (
    <>
      <motion.div
-       className="fixed top-0 bottom-0 right-full h-screen w-screen z-30 bg-gradient-to-r from-[#b9b4e9] via-[#b191f1] to-[#f1a5fd]"
+       className="fixed top-0 bottom-0 right-full h-screen w-screen z-30 bg-gradient-to-r from-[#b9b4e9] via-[#b191f1] to-[#f1a5fd] "
        initial={{ x: "100%", width: "100%" }}
        animate={{ x: "0%", width: "0%" }}
        exit={{ x: ["0%", "100%"], width: ["0%", "100%"] }}
